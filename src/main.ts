f595b2ac8aed3daa14d6d9045f62b85f3c4c6f37
armornick <nick.arnoeyts@mail.com>
2022-06-23 08:19:42 +0200

    add example for setting values

--- src/main.ts
+++ src/main.ts
@@ -6,4 +6,5 @@ $('.hide-me').hide();
 $('#hack-button').click(() => $('.hide-me').toggle());
 $('#fade-button').click(() => $('.fade-me').fadeOut());
 $('#get-text-button').click(() => alert($('h1').text()))
+$('#fix-button').click(() => $('h1').html('Lorem Ipsum'));
 
